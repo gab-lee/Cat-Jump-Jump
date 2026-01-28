1a6dbfb5585e84bce1c6b077cafc89b22bc575f1
Gab <53164692+gab-lee@users.noreply.github.com>
2026-01-28 14:19:18 +0800

    added scaling of window

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 
-W, H, L, R = 1200, 800, 150, 80
+W, H, L, R = 1280, 720, 300, 80
 
 #Game setup
 root = tk.Tk()
 root.title("Cat jump jump")
 root.geometry(f"{W}x{H}+{L}+{R}")
-canvas = tk.Canvas(root, width=0.8*W, height=0.8*H, bg="white")
+canvas = tk.Canvas(root, width = W, height=H, bg="white")
 canvas.pack(fill="both", expand=True)
 
-cat = canvas.create_oval(200,600,400,400)
+cat = canvas.create_oval(0.1*W,0.8*H,0.3*W,0.6*H)
 ground = canvas.create_rectangle(0,600,1200,800, fill="Blue")
 
 #'q': quit game
@@ -23,8 +23,15 @@ def jump(event):
     print("jumping")
 root.bind("<space>",jump)
 
-#keyboard binding
+#scaling window 
+def on_resize(event):
+    width = event.width
+    height = event.height
+    canvas.coords(ground, 0,0.8*height,width, height)
+    canvas.coords(cat, 0.1*width,0.8*height,0.2*width, 0.7*height)
+    print(width, height)
 
+root.bind("<Configure>", on_resize)
 
 
 
