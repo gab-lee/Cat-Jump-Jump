6b994d8a0df5223df756f44d6dc715e6167e2313
Gab <53164692+gab-lee@users.noreply.github.com>
2026-01-28 14:02:53 +0800

    added floor and cat

--- main.py
+++ main.py
@@ -6,10 +6,31 @@ W, H, L, R = 1200, 800, 150, 80
 root = tk.Tk()
 root.title("Cat jump jump")
 root.geometry(f"{W}x{H}+{L}+{R}")
+canvas = tk.Canvas(root, width=0.8*W, height=0.8*H, bg="white")
+canvas.pack(fill="both", expand=True)
 
+cat = canvas.create_oval(200,600,400,400)
+ground = canvas.create_rectangle(0,600,1200,800, fill="Blue")
+
+#'q': quit game
 def quit_game(event):
     root.destroy()
-
 root.bind("q", quit_game)
 
+#'space': jump
+def jump(event):
+    canvas.coords(cat, 200,400,400,600)
+    print("jumping")
+root.bind("<space>",jump)
+
+#keyboard binding
+
+
+
+
+
+
+
+
 root.mainloop()
+
